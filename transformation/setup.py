4036bb77e44fa8ac8b287b5cc16b10e3dbe3b66a
Matt Harrison <matt.harrison91@gmail.com>
2013-09-24 20:49:00 +0100

    Update setup

--- transformation/setup.py
+++ transformation/setup.py
@@ -1,6 +1,11 @@
 import math
+
+# temporary eastings and northing for testing
+
+latitude = 52.658007833
+longitude = 1.716073973 
+
 class projection_constant(object):
-	"""docstring for projection_constant"""
 	def __init__(self, F_0, phi_0, lambda_0, E_0, N_0):
 		super(projection_constant, self).__init__()
 		self.F_0 = F_0
@@ -11,20 +16,25 @@ class projection_constant(object):
 		
 
 class ellipsoid(object):
-	"""docstring for ellipsoid"""
-	def __init__(self, a, b):
+	def __init__(self, a, b, description):
 		self.a = a
 		self.b = b
-		self.e_sq = (math.pow(a,2) - math.pow(b,2)) / math.pow(a,2)
-		self.n = (a-b)/(a+b)
+		self.description = description
+		self.e_sq = (a**2 - b**2) / a**2
 
 
 national_grid = projection_constant(0.9996012717, math.radians(49), math.radians(2), 400000, -100000)
 
+airy_1830 = ellipsoid(6377563.396,6356256.910, "OSGB36 National Grid")
+GRS80 = ellipsoid(6378137.0, 6356752.314, "ETRS89 (WGS84)")
 
+def lat_long_to_east_north(lat,long,ellipsoid_used,projection_used):
+	n = (ellipsoid_used.a - ellipsoid_used.b) / (ellipsoid_used.a + ellipsoid_used.b)
+	v = ellipsoid_used.a * projection_used.F_0 * (1-(ellipsoid_used.e_sq*(math.sin(lat)**2)))**-0.5
+	print v
+	
+lat_long_to_east_north(latitude,longitude,GRS80,national_grid)
 
-airy_1830 = ellipsoid(6377563.396,6356256.910)
-GRS80 = ellipsoid(6378137.0, 6356752.314)
 
 
 print airy_1830.e_sq
