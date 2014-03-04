d3f1947f79882f1d132991a054df1758546442b4
Matt Harrison <matt.harrison91@gmail.com>
2014-03-04 13:26:46 +0000

    Update tests so they are working again

--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -1,19 +1,20 @@
-from transformation.OSTN02 import OSGB36_to_ETRS89, ETRS89_to_OSGB36, GRS80, national_grid
-from transformation.ll_en_converter import lat_long_to_east_north, east_north_to_lat_long
 import math
+
+from transformation.transformation.OSTN02 import OSGB36_to_ETRS89, ETRS89_to_OSGB36, GRS80, national_grid
+from transformation.transformation.ll_en_converter import lat_long_to_east_north, east_north_to_lat_long
 
 
 def test_OSGB36_to_ETRS89():
-    lat, long = 651409.792, 313177.448
-    e, n = OSGB36_to_ETRS89(lat, long)
-    x, y = east_north_to_lat_long(e, n, GRS80, national_grid)
-    assert round(x, 8) == 52.65800783
-    assert round(y, 8) == 1.71607397
+    e, n, h = 651409.792, 313177.448, 105.6
+    x, y, z = OSGB36_to_ETRS89(e, n, h)
+    lat, long = east_north_to_lat_long(x, y, GRS80, national_grid)
+    assert round(lat, 8) == 52.65800783
+    assert round(long, 8) == 1.71607397
 
 
 def test_ETRS89_to_OSGB36():
-    x, y = 52.658007833, 1.716073973
-    x, y = lat_long_to_east_north(math.radians(x), math.radians(y), GRS80, national_grid)
-    e, n = ETRS89_to_OSGB36(x, y)
+    lat, long, z = 52.658007833, 1.716073973, 149.844
+    x, y = lat_long_to_east_north(math.radians(lat), math.radians(long), GRS80, national_grid)
+    e, n, h = ETRS89_to_OSGB36(x, y, z)
     assert round(e, 3) == 651409.792
     assert round(n, 3) == 313177.448
